fad7776edb232adb5ac0181e1852b7496edc5596
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2021-10-17 19:47:11 +0200

    Make it tick

--- src/hooks/use-cell-pool.ts
+++ src/hooks/use-cell-pool.ts
@@ -1,6 +1,22 @@
-import { createRandomlyPopulatedCellPool } from "../utils/cell-pool.utils";
+import { useEffect, useState } from "react";
+import {
+  createRandomlyPopulatedCellPool,
+  generateNewCellPoolOnTick,
+} from "../utils/cell-pool.utils";
 
 export const useCellPool = () => {
-  const cellPool = createRandomlyPopulatedCellPool();
+  const [cellPool, setCellPool] = useState<number[][]>([]);
+
+  useEffect(() => {
+    const initialCellPool = createRandomlyPopulatedCellPool();
+    setCellPool(initialCellPool);
+
+    setInterval(() => {
+      setCellPool((currentCellPool) =>
+        generateNewCellPoolOnTick(currentCellPool)
+      );
+    }, 400);
+  }, []);
+
   return cellPool;
 };
--- src/utils/cell-pool.utils.ts
+++ src/utils/cell-pool.utils.ts
@@ -11,3 +11,65 @@ export const createRandomlyPopulatedCellPool = (): number[][] =>
         : CellState.ALIVE
     )
   );
+
+export const generateNewCellPoolOnTick = (cellPool: number[][]): number[][] => {
+  const newCellPool: number[][] = [];
+
+  for (let i = 0; i < 50; i++) {
+    newCellPool[i] = newCellPool[i] ?? [];
+
+    for (let j = 0; j < 50; j++) {
+      const amountOfLiveNeighbours = getAmountOfLiveNeighbours(cellPool, i, j);
+
+      newCellPool[i][j] = getNewCellState(
+        cellPool[i][j],
+        amountOfLiveNeighbours
+      );
+    }
+  }
+
+  return newCellPool;
+};
+
+const getAmountOfLiveNeighbours = (
+  cellPool: number[][],
+  i: number,
+  j: number
+): number => {
+  let amount = 0;
+
+  if (cellPool[i - 1]) {
+    amount +=
+      (cellPool[i - 1][j - 1] ?? 0) +
+      (cellPool[i - 1][j] ?? 0) +
+      (cellPool[i - 1][j + 1] ?? 0);
+  }
+
+  amount += (cellPool[i][j - 1] ?? 0) + (cellPool[i][j + 1] ?? 0);
+
+  if (cellPool[i + 1]) {
+    amount +=
+      (cellPool[i + 1][j - 1] ?? 0) +
+      (cellPool[i + 1][j] ?? 0) +
+      (cellPool[i + 1][j + 1] ?? 0);
+  }
+
+  return amount;
+};
+
+const getNewCellState = (
+  cellState: CellState,
+  amountOfLiveNeighbours: number
+): CellState => {
+  if (cellState === CellState.ALIVE) {
+    if (amountOfLiveNeighbours < 2 || amountOfLiveNeighbours > 3) {
+      return CellState.DEAD;
+    } else {
+      return cellState;
+    }
+  } else if (amountOfLiveNeighbours === 3) {
+    return CellState.ALIVE;
+  } else {
+    return cellState;
+  }
+};
