3982bf2c26ed21bb61250db7237aa2748b236daf
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2021-10-17 18:52:59 +0200

    Render cell pool

--- src/components/cell-pool/cell-pool.tsx
+++ src/components/cell-pool/cell-pool.tsx
@@ -1,5 +1,29 @@
+import classNames from "classnames";
+
 import "./cell-pool.css";
 
+enum CellState {
+  ALIVE = 1,
+  DEAD = 0,
+}
+
 export const CellPool = () => {
-  return <div className="pool"></div>;
+  const cellPool: number[][] = [[]];
+
+  return (
+    <div className="pool">
+      {cellPool.map((row, rowIndex) =>
+        row.map((state, colIndex) => (
+          <div
+            className={classNames("pool__cell", {
+              "pool__cell--alive": state === CellState.ALIVE,
+            })}
+            key={`${rowIndex}${colIndex}`}
+          >
+            &nbsp;
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
